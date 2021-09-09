60427cdf77a10eaae1d75678ffb41d2b17c37956
petr <peta.hlavnicku@gmail.com>
2021-09-09 10:15:53 +0200

    Try to call onFaceScanResultCancel when success is true

--- ios/LivenessCheckProcessor.swift
+++ ios/LivenessCheckProcessor.swift
@@ -129,6 +129,7 @@ class LivenessCheckProcessor: NSObject, Processor, FaceTecFaceScanProcessorDeleg
                 // In v9.2.0+, simply pass in scanResultBlob to the proceedToNextStep function to advance the User flow.
                 // scanResultBlob is a proprietary, encrypted blob that controls the logic for what happens next for the User.
                 self.success = true
+                faceScanResultCallback.onFaceScanResultCancel()
 //                 self.success = faceScanResultCallback.onFaceScanGoToNextStep(scanResultBlob: scanResultBlob)
             }
             else {
